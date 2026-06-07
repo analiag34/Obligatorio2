5afa9f06c0d8fefb4fb08614805598405b2400d2
americacaceres <americacaceres10@gmail.com>
2026-06-07 14:04:51 -0300

    add web project

---
+++ Web/Controllers/AuthController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Web.Models;
+
+namespace Web.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Login(LoginViewModel lvm)
+        {
+            return View();
+        }
+        public IActionResult Logout()
+        {
+            return View();
+        }
+    }
+}
