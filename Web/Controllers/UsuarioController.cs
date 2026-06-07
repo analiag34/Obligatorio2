9e7fe5c844cabab6f2af620ca4f614413c7a56f4
Analia Felix <annegomez4456@gmail.com>
2026-06-07 19:27:22 -0300

    Creacion de Instancia en Sistema, atributo rol en persona, metodo Alta Persona en Controller usuario y vista

---
+++ Web/Controllers/UsuarioController.cs
@@ -0,0 +1,32 @@
+﻿using Dominio;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        Sistema s = Sistema.Instance();
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Persona nueva)
+        {
+            try
+            {
+                s.AltaPersona(nueva);
+                ViewBag.msg = "Alta correcta";
+            }
+            catch (Exception e)
+            {
+                ViewBag.msg = e.Message;
+            }
+            return View();
+
+        }
+    }
+}
